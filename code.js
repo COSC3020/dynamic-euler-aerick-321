7fae374626b6ea05eefcbbf3457046ebba122315
aerick-321 <158105735+aerick-321@users.noreply.github.com>
2024-12-02 16:10:26 -0700

    Update code.js

--- code.js
+++ code.js
@@ -4,9 +4,11 @@ function factorial(n) {
 }
 
 function e(n) {
+    let k = 0;
     let fact = 1;
     let result = 1;
-    for(let k = 1; k <= n; k++){
+    while( k <= n;){
+        k++;
         fact = k * fact;
         result = result + (1/fact);
     }
